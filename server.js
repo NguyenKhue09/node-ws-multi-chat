7ce36774bcb553c624329da858de90bc2453d10d
NguyenKhue09 <19521712@gm.uit.edu.vn>
2021-03-04 21:26:30 +0700

    cant sent userName

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const http = require('http');
 const WebSocket = require('ws');
 
 const port = 6969;
-const server = http.createServer(express);
+const server = http.createServer(express); // https://www.w3schools.com/nodejs/met_http_createserver.asp
 const wss = new WebSocket.Server({ server })
 
+// on(event: 'connection', cb: (this: Server, socket: WebSocket, request: http.IncomingMessage) => void): this;
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
+  // on(event: 'message', listener: (this: WebSocket, data: WebSocket.Data) => void): this;
+  ws.on('message', function incoming( data ) {
+    console.log("tin nhan " + data);
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(data);
@@ -16,7 +19,8 @@ wss.on('connection', function connection(ws) {
   })
 })
 
+// https://www.w3schools.com/nodejs/met_server_listen.asp
 server.listen(port, function() {
   console.log(`Server is listening on ${port}!`)
 })
-// abc
+
